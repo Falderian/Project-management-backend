0587927de7046e510ba436eff18c3aed3cdf13d1
Falderian <nikitastaselovich@gmail.com>
2022-11-09 13:36:54 +0300

    feat: add user

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,10 +3,14 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
+const UserName = 'admin';
+const Password = 'admin';
+const clasterInfo = 'cluster0.gcgejjl';
+const applicationName = 'project-management-backend';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/${applicationName}');
+    await mongoose.connect(`mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/${applicationName}`);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
